dce17a741cdd2d28cc7eea8f91da1eeb3fc70afa
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2021-01-05 13:06:16 +0900

    using process?

--- compare_frames.py
+++ compare_frames.py
@@ -8,53 +8,59 @@ from tqdm import tqdm
 
 SENTINEL = -1
 
-def compare_framenum(input_path, output_path, Q):
-    nb_input_frames = int(subprocess.run(
-        [
-            'ffprobe', 
-            '-v', 
-            'fatal', 
-            '-count_frames',
-            '-select_streams',
-            'v:0',
-            '-show_entries', 
-            'stream=nb_read_frames', 
-            '-of', 
-            'default=noprint_wrappers=1:nokey=1', 
-            str(input_path)
-        ], 
-        stdout=subprocess.PIPE, 
-        stderr=subprocess.STDOUT).stdout
-    )
-    nb_output_frames = int(subprocess.run(
-        [
-            'ffprobe', 
-            '-v', 
-            'fatal', 
-            '-count_frames',
-            '-select_streams',
-            'v:0',
-            '-show_entries', 
-            'stream=nb_read_frames', 
-            '-of', 
-            'default=noprint_wrappers=1:nokey=1', 
-            str(output_path)
-        ], 
-        stdout=subprocess.PIPE, 
-        stderr=subprocess.STDOUT).stdout
-    )  
-    vid_name = Path(input_path).name
-    Q.put(vid_name)
-    return nb_input_frames == nb_output_frames
+def compare_framenum(input_paths, output_paths, Q):
+    results = []
+    for input_path, output_path in zip(input_paths, output_paths):
+        nb_input_frames = int(subprocess.run(
+            [
+                'ffprobe', 
+                '-v', 
+                'fatal', 
+                '-count_frames',
+                '-select_streams',
+                'v:0',
+                '-show_entries', 
+                'stream=nb_read_frames', 
+                '-of', 
+                'default=noprint_wrappers=1:nokey=1', 
+                str(input_path)
+            ], 
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.STDOUT).stdout
+        )
+        nb_output_frames = int(subprocess.run(
+            [
+                'ffprobe', 
+                '-v', 
+                'fatal', 
+                '-count_frames',
+                '-select_streams',
+                'v:0',
+                '-show_entries', 
+                'stream=nb_read_frames', 
+                '-of', 
+                'default=noprint_wrappers=1:nokey=1', 
+                str(output_path)
+            ], 
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.STDOUT).stdout
+        )  
+        vid_name = Path(input_path).name
+        Q.put((vid_name, nb_input_frames==nb_output_frames))
 
-def tqdm_counter(Q, length):
+
+def tqdm_counter(Q, final_Q, length):
     t = tqdm(total=length, unit='videos')
+    result_list = []
     while True:
-        vid_name = Q.get()
-        if vid_name == SENTINEL:
+        result = Q.get()
+        if result == SENTINEL:
+            final_Q.put(result_list)
             t.close()
             return
         else:
+            result_list.append(result)
+            vid_name, is_same = result
             t.update()
             t.set_description(vid_name)
 
@@ -71,17 +77,32 @@ if __name__ == '__main__':
     vid_names = os.listdir(vid_dir)
     vid_names.sort()
     done_Q = Queue()
+    final_Q = Queue()
 
-    tqdm_proc = Process(target=tqdm_counter, args=(done_Q, len(vid_names)))
+    tqdm_proc = Process(target=tqdm_counter, 
+                    args=(done_Q, final_Q, len(vid_names)))
     tqdm_proc.start()
-    with Pool(processes=args.parallel) as pool:
-        is_same = pool.starmap(compare_framenum, 
-                            zip([vid_dir/v for v in vid_names],
-                                [edge_dir/v for v in vid_names],
-                                [done_Q]*len(vid_names)))
+
+    compare_procs = []
+    per_worker = len(vid_names)//args.parallel
+    for i in range(args.parallel):
+        compare_procs.append(Process(
+            target=compare_framenum,
+            args=(
+                [vid_dir/vn for vn in vid_names],
+                [edge_dir/vn for vn in vid_names],
+                done_Q,
+            )
+        ))
+    for p in compare_procs:
+        p.start()
+    for p in compare_procs:
+        p.join()
+
     done_Q.put(SENTINEL)
     tqdm_proc.join()
-
-    for v, b in zip(vid_names, is_same):
+    results = final_Q.get()
+    print('Not same vids:')
+    for v, b in results:
         if not is_same:
             print(v)
